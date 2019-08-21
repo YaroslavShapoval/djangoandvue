d7b3ce9bc1b108e6b167ed80dbc8ba409457888c
Yaroslav Shapoval <shapoval.yaroslav@gmail.com>
2019-08-21 21:43:45 +0300

    Use named routes inside the navbar

--- frontend/src/components/CustomNavBar.vue
+++ frontend/src/components/CustomNavBar.vue
@@ -2,24 +2,24 @@
   <div class="navbar-wrapper">
     <b-navbar toggleable="lg" type="light" variant="light">
       <div class="container">
-        <b-navbar-brand to="/">Django And Vue</b-navbar-brand>
+        <b-navbar-brand :to="{ name: 'home' }">Django And Vue</b-navbar-brand>
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
-            <b-nav-item to="/">Home</b-nav-item>
-            <b-nav-item v-if="loggedIn" to="/reviews">Reviews</b-nav-item>
-            <b-nav-item v-if="loggedIn" to="/reviews/stats">Stats</b-nav-item>
+            <b-nav-item :to="{ name: 'home' }">Home</b-nav-item>
+            <b-nav-item v-if="loggedIn" :to="{ name: 'reviews' }">Reviews</b-nav-item>
+            <b-nav-item v-if="loggedIn" :to="{ name: 'reviews-stats' }">Stats</b-nav-item>
 
             <b-nav-item-dropdown v-if="loggedIn" right>
               <!-- Using 'button-content' slot -->
               <template slot="button-content"><em>Account</em></template>
-              <b-dropdown-item to="/logout">Sign Out</b-dropdown-item>
+              <b-dropdown-item :to="{ name: 'logout' }">Sign Out</b-dropdown-item>
             </b-nav-item-dropdown>
 
-            <b-nav-item v-if="!loggedIn" to="/login">Login</b-nav-item>
+            <b-nav-item v-if="!loggedIn" :to="{ name: 'login' }">Login</b-nav-item>
           </b-navbar-nav>
         </b-collapse>
       </div>
