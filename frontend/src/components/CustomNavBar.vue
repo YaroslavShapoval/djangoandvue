f6bc494b15aa7ab69ba26e044453b37208fcac96
Yaroslav Shapoval <shapoval.yaroslav@gmail.com>
2019-08-21 22:49:11 +0300

    Add ReviewCreateComponent

--- frontend/src/components/CustomNavBar.vue
+++ frontend/src/components/CustomNavBar.vue
@@ -2,15 +2,16 @@
   <div class="navbar-wrapper">
     <b-navbar toggleable="lg" type="light" variant="light">
       <div class="container">
-        <b-navbar-brand :to="{ name: 'home' }">Django And Vue</b-navbar-brand>
+        <b-navbar-brand to="/">Django And Vue</b-navbar-brand>
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
-            <b-nav-item :to="{ name: 'home' }">Home</b-nav-item>
+            <b-nav-item to="/">Home</b-nav-item>
             <b-nav-item v-if="loggedIn" :to="{ name: 'reviews' }">Reviews</b-nav-item>
+            <b-nav-item v-if="loggedIn" :to="{ name: 'review-add' }">Add review</b-nav-item>
             <b-nav-item v-if="loggedIn" :to="{ name: 'reviews-stats' }">Stats</b-nav-item>
 
             <b-nav-item-dropdown v-if="loggedIn" right>
